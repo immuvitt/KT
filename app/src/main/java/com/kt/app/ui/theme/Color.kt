38c622cb029684cb1989c09322cfb9dbc8da549e
immuvitt <immuvitt@gmail.com>
2026-08-23 23:08:11 +0530

    Update Color.kt

--- app/src/main/java/com/kt/app/ui/theme/Color.kt
+++ app/src/main/java/com/kt/app/ui/theme/Color.kt
@@ -1,5 +1,12 @@
 package com.kt.app.ui.theme
+
 import androidx.compose.ui.graphics.Color
-val KtBg=Color(0xFF05070A)
-val KtCyan=Color(0xFF00E5FF)
-val KtGold=Color(0xFFFFC107)
+
+// Kuber Tijori Theme
+val Purple80 = Color(0xFF18D8F5)      // Cyan
+val PurpleGrey80 = Color(0xFFB8C2D6)  // Silver
+val Pink80 = Color(0xFF0A1328)        // Navy
+
+val Purple40 = Color(0xFF00CFEA)      // Cyan
+val PurpleGrey40 = Color(0xFF8E99B3)  // Grey Silver
+val Pink40 = Color(0xFFFFC400)        // Gold Accent
