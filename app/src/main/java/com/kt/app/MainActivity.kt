5491ec4fa1ee1a65bf99e87eb0f4f6b7b55f8acc
Imran <immuvitt@gmail.com>
2026-08-24 21:06:59 +0530

    KT_Sprint1B_Integrated_API_HUD

--- app/src/main/java/com/kt/app/MainActivity.kt
+++ app/src/main/java/com/kt/app/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
