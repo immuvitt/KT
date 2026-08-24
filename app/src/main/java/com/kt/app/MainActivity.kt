c81eeb71ef0699c66b445d3f297ec2ded26e661c
Imran <immuvitt@gmail.com>
2026-08-24 11:24:55 +0530

    paper trading changes

--- app/src/main/java/com/kt/app/MainActivity.kt
+++ app/src/main/java/com/kt/app/MainActivity.kt
@@ -22,7 +22,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddChart
